8658c60eae8881ef1c23d6e753bd904e3ee97c12
Chinedu Ukpe <cukpe@emgeresources.com>
2021-08-06 14:45:04 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chinedu Ukpe</title>
+    <link id="themeStylesheet" rel="stylesheet" href="static/css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="brandWrapper">
+            <!-- <a href="/"><span>SLID</span>Nigeria</a> -->
+            <a href="#main">Portfolio</a>
+        </div>
+        <ul>
+            <li><a href="#main">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#achievements">Works</a></li>
+        </ul>
+        <a href="javascript:;" class="nav-toggle"> 
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </nav>
+
+    <main id="main">
+        <div class="landingBanner">
+            <div class="titleWrapper">
+                <div>
+                    <div class="block"></div>
+                    <h1>Ukpe</h1>
+                    <h1>Chinedu</h1>
+                    <p><i class="fa fa-briefcase"></i> <strong>Fullstack Developer</strong> </p>
+                    <p>Chinedu is a seasoned web developer with passion for technology. With over four years' professional experience in web technologies, cloud computing and systems design. Chinedu is also passionate about Artificial Intelligence (AI) and Machine Learning (ML).</p>
+                    <a href="javascript:;" class="bannerLink">Read more about me <i class="fa fa-arrow-right"></i></a>
+                </div>
+            </div>
+            <div class="imageWrapper">
+                <img src="static/img/profile-img.png" alt="">
+            </div>
+        </div>
+        
+        <div class="scene products" id="about">
+            <div class="technologies">
+                <img src="static/img/icons/aws.png" alt="">
+                <img src="static/img/icons/laravel.png" alt="">
+                <img src="static/img/icons/node-js.png" alt="">
+                <img src="static/img/icons/react-js.png" alt="">
+            </div>
+            <div class="experience-wrapper">
+                <div class="years">
+                    <h1 class="number">5+</h1>
+                    <p>Years Experience Working</p>
+                </div>
+                <div class="experties">
+                    <h4>Web design and development. Cloud deployment, CI/CD configuration and mobile expert.</h4>
+                    <div class="tech-stack">
+                        <div class="stack-item active">
+                            <i class="fa fa-sitemap"></i>
+                            <p>Web development</p>
+                            <a href="#contact-section">Discover More <i class="fa fa-arrow-right"></i></a>
+                        </div>
+                        <div class="stack-item">
+                            <i class="fa fa-cloud"></i>
+                            <p>Cloud deployment</p>
+                            <a href="#contact-section">Discover More <i class="fa fa-arrow-right"></i></a>
+                        </div>
+                        <div class="stack-item">
+                            <i class="fa fa-mobile-phone"></i>
+                            <p>Mobile Applications</p>
+                            <a href="#contact-section">Discover More <i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="scene achievements" id="achievements"> 
+            <hgroup>
+                <h1 class="title">Achievements</h1>
+                <h2 class="subtitle">See how far I have come in this line of work. quite interesting</h2>
+            </hgroup>
+            <div class="stripe">
+                <h3> <i class="fa fa-briefcase"></i> 29 <span>Projects Completed</span></h3>
+            </div>
+            <div class="stripe">
+                <h3><i class="fa fa-cog"></i> 1 <span>Bachelor's degree</span></h3>
+                <p class="text-white">Benue State University, Makurdi - 2016</p>
+            </div>
+            <div class="stripe">
+                <h3><i class="fa fa-users"></i> 16+ <span>Clients satisfied</span></h3>
+            </div>
+        </div>
+        
+        <div class="scene bg-white contact-section" id="contact-section">
+            <div class="bubble"></div>
+            <h1 class="title text-primary">Send me a mail</h1>
+            <form action="" class="form">
+                <label for="">Fullname</label>
+                <input type="text">
+                <label for="">Email</label>
+                <input type="text">
+                <label for="">Phone</label>
+                <input type="text">
+                <label for="">Message</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <button type="button" class="section-button">Send mail <i class="fa fa-send"></i></button>
+            </form>
+        </div>
+        
+        <div style="padding: 2em;">
+            
+        </div>
+        
+        <div class="staticSocial">
+            <a target="_blank" href="https://github.com/chineduukpe"><i class="fa fa-github"></i></a>
+            <a target="_blank" href="https://linkedin.com/in/chinedu-ukpe"><i class="fa fa-linkedin"></i></a>
+            <!-- <a target="_blank" href="https://facebook.com/ukpe.friday1"><i class="fa fa-facebook"></i></a> -->
+            <a target="_blank" href="https://twitter.com/chineduukpe"><i class="fa fa-twitter"></i></a>
+            <a target="_blank" href="https://instagram.com/chinedu_uf"><i class="fa fa-instagram"></i></a>
+        </div>
+        
+        <div class="theme-selector">
+            <a href="javascript"><i class="fa fa-cog"></i> Select theme</a>
+            <div class="theme-types">
+                <a href="javascript:;" data-theme-url="static/css/style.css"><i class="fa fa-sun"></i>Light</a>
+                <a href="javascript:;"  data-theme-url="static/css/style-dark.css"><i class="fa fa-moon"></i>dark</a>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="logo-wrapper">
+            <img src="static/img/slid-logo1.jpg" alt="">
+        </div>
+        <div class="services-wrapper">
+            <div class="designs-wrapper">
+                <h3 class="text-center text-white">Web Designs</h3>
+                <ul>
+                    <li><a target="_blank" href="http://slidlanding-1.herokuapp.com/login1.html">Login page design</a></li>
+                    <li><a target="_blank" href="http://sliddashboard-1.herokuapp.com/dashboard.php">Dashboard Design</a></li>
+                    <li><a target="_blank" href="http://slidlanding-1.herokuapp.com/dashbaord/index.html">Dashboard Design 2</a></li>
+                    <li><a target="_blank" href="http://slidlanding-1.herokuapp.com/index3.html">Landing page design</a></li>
+                </ul>
+            </div>
+            <div class="designs-wrapper">
+                <h3 class="text-center text-white">Other designs</h3>
+                <ul>
+                    <!-- <li><a href="http://sliddashboard-1.herokuapp.com/">Login page design 2</a></li> -->
+                    <li><a target="_blank" href="http://sliddashboard-1.herokuapp.com/">Login page design 2</a></li>
+                    <li><a target="_blank" href="http://slidlanding-1.herokuapp.com/share.html">Share page design</a></li>
+                </ul>
+            </div>
+        </div>
+        
+    </footer>
+    <p class="copyright">
+        &copy; Chinedu Ukpe 2021
+    </p>
+
+    <script src="https://use.fontawesome.com/c4c1bccc3a.js"></script>
+    <script>
+        const themeStylesheet = document.querySelector('#themeStylesheet');
+
+        const themeSelectors = document.querySelectorAll('.theme-selector .theme-types a');
+        
+        const onThemeChange = e => {
+            console.log(e)
+            themeStylesheet.setAttribute('href', e.target.getAttribute('data-theme-url'))
+        }
+
+        themeSelectors.forEach(e => e.addEventListener('click',onThemeChange))
+
+        const achievementsSection = document.querySelector('.achievements');
+
+        const nav = document.querySelector('nav')
+
+        const html = document.querySelector('html');
+
+        const navToggle = document.querySelector('.nav-toggle');
+
+        const mobileNav = document.querySelector('nav ul');
+
+        const onWindowScroll = e => {
+            if(window.scrollY > (achievementsSection.offsetHeight )){
+                html.style.backgroundImage = 'url(../static/img/bg/bg3.jpg)'
+            }else{
+                html.style.backgroundImage = 'url(../static/img/bg/bg001.jpeg)'
+
+            }
+
+            /* ADD AN ACTIVE CLASS TO NAV TO CHANGE BACKGROUND COLOR */
+            if (window.scrollY > 100) {
+                nav.classList.add('active')
+            }else{
+                nav.classList.remove('active')
+            }
+        }
+
+        const toggleNav = () => {
+            if(navToggle.classList.contains('active')){
+                mobileNav.classList.remove('mobile-active')
+                return navToggle.classList.remove('active')
+
+            }
+
+            mobileNav.classList.add('mobile-active')
+            return navToggle.classList.add('active')
+
+        }
+
+        document.addEventListener('scroll',onWindowScroll)
+
+        navToggle.addEventListener('click', toggleNav);
+    </script>
+</body>
+</html>
